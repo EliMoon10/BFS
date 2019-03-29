acfd6cc4f9654f08b2e7be6c3ed3c509d4987ffc
GijunMoon <gijunmoon@moonstudio.kro.kr>
2019-03-29 22:49:02 +0900

    Update Code (It's work!)

--- Breadth-First Search(BFS)/Breadth-First Search(BFS)/Breadth-First Search(BFS).cpp
+++ Breadth-First Search(BFS)/Breadth-First Search(BFS)/Breadth-First Search(BFS).cpp
@@ -1,4 +1,4 @@
-﻿/*2019/02/19 EliMoon 
+/*2019/02/19 EliMoon : Update 2019/03/29
   Just Test Code
   Enjoy! */
 
@@ -6,44 +6,62 @@
 깊이 탐색방식(DFS)와 같이 사용되는 너비 탐색방식 알고리즘 입니다.
 그래프의 인접 노드를 차례대로 방문하며 전체 탐색을 진행합니다.*/
 
-#include "pch.h"
 #include <iostream>
-#include <vector> //vector를 사용하기 위함
-#include <queue> //queue를 사용하기 위함
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
-#define g 8 //배열 정의
+using index_t = std::vector<int>::size_type;
 
-vector<int> list[g]; //그래프와의 연결관계 (그래프를 담기 위함)
-int visit[g]; //노드 g를 방문한 적이 있으면 1이고 아니면 0 (방문한 노드를 담기 위함)
-
-
-int main()
-{
+void bfs(int start, vector<int> graph[], bool check[]) {
 	queue<int> q;
 
-	int node = 1; //시작노드를 1로
-	q.push(node); //시작 노드를 큐에다 push
-	visit[node] = 1; //시작노드를 방문했음
+	q.push(start);
+	check[start] = true;
 
-	while (!q.empty()) //큐가 비어있지 않을 동안 반복
-	{
-
-		int it = q.front(); //큐의 가장 앞애 있는 노드
-		q.pop(); //25라인의 it를 큐에 pop
-
-		for (int j = 0; j < list[it].size(); j++) //가장 인접한 노드중
-		{
-			int there = list[it][j];
-
-			if (visit[it] == 0) //아직 방문하지 않은 노드를 큐에 push
-			{
-				q.push(it);
-				visit[it] = 1;
+	while (!q.empty()) {
+		int tmp = q.front();
+		q.pop();
+		printf("%d ", tmp);
+		for (vector<int>::size_type i = 0; i < graph[tmp].size(); i++) {
+			if (check[graph[tmp][i]] == false) {
+				q.push(graph[tmp][i]);
+				check[graph[tmp][i]] = true;
 			}
 		}
-		//반복~
 	}
+
 }
 
+int main() {
 
+	int n = 2;
+	int m = 3;
+	int start = 1;
+	//cin >> n >> m >> start;
+
+	vector<int> graph[n + 1];
+	bool check[n + 1];
+	fill(check, check + n + 1, false);
+
+	for (int i = 0; i < m; i++) {
+		int u, v;
+		cin >> u >> v;
+
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+	// Sorting사용 이유: 차후 node를 순차적으로 접근해야하기 때문
+	for (int i = 1; i <= n; i++) {
+		sort(graph[i].begin(), graph[i].end());
+	}
+
+	//bfs
+	bfs(start, graph, check);
+	printf("\n");
+
+	return 0;
+}
